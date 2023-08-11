423c6b12b31357bb11537ed27463aef558ede2cd
willychap <wchapman@ucsd.edu>
2023-08-11 13:43:23 -0600

    port from other repo

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MJOcast',
+    version='0.1.0',
+    author='William E. Chapman',
+    author_email='wchapman@ucar.edu',
+    description='A package to create the forecasting of MJO indices',
+    license='GNU General Public License v2',
+    packages=find_packages(include='WH_RMM_forecasting*'),
+    install_requires=[
+        'pyyaml',
+        'pandas',#pandas 2.0? 
+        'numpy',
+        'matplotlib',
+        'xarray',
+        'eofs',
+        'datetime',
+        'scipy',
+        'netCDF4'
+    ],
+    extras_require={
+        'full_func': ['jupyter','dask'],
+        'dev': ['build', 'pytest', 'pytest-pep8'],
+      },
+    package_data={'MJOcast': ['*/*.nc']},
+)
